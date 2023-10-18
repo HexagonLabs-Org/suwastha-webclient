00cd90e1b8a48d8017e3f33c757fb7cb3440168c
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-18 18:43:23 +0530

    config routings for patient CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { AuthComponent } from './components/auth/auth.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { PatientListComponent } from './components/patient-CRUD/patient-list/patient-list.component';
+import { PatientAddComponent } from './components/patient-CRUD/patient-add/patient-add.component';
+import { PatientDetailsComponent } from './components/patient-CRUD/patient-details/patient-details.component';
+import { PatientEditComponent } from './components/patient-CRUD/patient-edit/patient-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -14,8 +18,12 @@ const routes: Routes = [
     component: DashboardComponent,
     children: [
       { path: '', redirectTo: 'profile', pathMatch: 'full' },
-      { path: 'profile', component:  ProfileComponent},
-      { path: 'settings', component: SettingsComponent }
+      { path: 'profile', component: ProfileComponent },
+      { path: 'settings', component: SettingsComponent },
+      { path: 'patients', component: PatientListComponent },
+      { path: 'patients/add', component: PatientAddComponent },
+      { path: 'patients/details/:id', component: PatientDetailsComponent },
+      { path: 'patients/edit/:id', component: PatientEditComponent }
     ]
   }
 ];
