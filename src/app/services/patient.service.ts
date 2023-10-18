f88e109764954500a9bbfe9983ad567bdb085818
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-18 18:42:19 +0530

    implemented patient service

---
+++ src/app/services/patient.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Patient } from '../Models/Entities/patient.model';
+import { UpdatePatientRequest } from '../Models/DTOs/update-patient-request.model';
+import { AddPatientRequest } from '../Models/DTOs/add-patient-request.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+  private apiUrl = 'http://suwasthaservice-001-site1.htempurl.com/Patient'; // Update this to your API endpoint
+
+  constructor(private http: HttpClient) { }
+
+  getAllPatients(): Observable<Patient[]> {
+    return this.http.get<Patient[]>(`${this.apiUrl}`);
+  }
+
+  getPatientById(id: string): Observable<Patient> {
+    return this.http.get<Patient>(`${this.apiUrl}/${id}`);
+  }
+
+  addPatient(patient: AddPatientRequest): Observable<Patient> {
+    return this.http.post<Patient>(`${this.apiUrl}`, patient);
+  }
+
+  updatePatient(id: string, patient: UpdatePatientRequest): Observable<Patient> {
+    return this.http.put<Patient>(`${this.apiUrl}/${id}`, patient);
+  }
+
+  deletePatient(id: string): Observable<Patient> {
+    return this.http.delete<Patient>(`${this.apiUrl}/${id}`);
+  }
+}
