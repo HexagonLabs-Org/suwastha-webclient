74cedc4d72ded49c2453c7c2eeb7075a1105e42f
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-21 02:19:54 +0530

    implemented doctor-list component

--- src/app/components/doctor-CRUD/doctor-list/doctor-list.component.ts
+++ src/app/components/doctor-CRUD/doctor-list/doctor-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Doctor } from 'src/app/Models/Entities/doctor.model';
+import { DoctorService } from 'src/app/services/doctor.service';
 
 @Component({
   selector: 'app-doctor-list',
   templateUrl: './doctor-list.component.html',
   styleUrls: ['./doctor-list.component.css']
 })
-export class DoctorListComponent {
+export class DoctorListComponent implements OnInit {
+  doctors: Doctor[] = [];
 
+  constructor(private doctorService: DoctorService) {}
+
+  ngOnInit() {
+    this.loadDoctorList();
+  }
+
+  loadDoctorList() {
+    this.doctorService.getAllDoctors().subscribe((doctors: Doctor[]) => {
+      this.doctors = doctors;
+    });
+  }
+
+  deleteDoctor(doctor: Doctor) {
+    // Implement the logic to delete the doctor using the service method
+    this.doctorService.deleteDoctor(doctor.id).subscribe(() => {
+      // Optionally, update the doctors list or perform other actions after deletion
+      this.loadDoctorList();
+    });
+  }
+  
 }
