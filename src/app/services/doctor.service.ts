40fb4f4bb1d24866a4adff84d52092e9d9be57c5
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-21 02:19:26 +0530

    implemented doctor service

--- src/app/services/doctor.service.ts
+++ src/app/services/doctor.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Doctor } from '../Models/Entities/doctor.model';
+import { AddDoctorRequest } from '../Models/DTOs/add-doctor-request.model';
+import { UpdateDoctorRequest } from '../Models/DTOs/update-doctor-request.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
+  private apiUrl = environment.apiUrl + '/Doctor'; // Update this to your API endpoint
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllDoctors(): Observable<Doctor[]> {
+    return this.http.get<Doctor[]>(`${this.apiUrl}`);
+  }
+
+  getDoctorByGuid(id: string): Observable<Doctor> {
+    return this.http.get<Doctor>(`${this.apiUrl}/${id}`);
+  }
+
+  getDoctorByDoctorID(doctorID: string): Observable<Doctor> {
+    return this.http.get<Doctor>(`${this.apiUrl}/${doctorID}`);
+  }
+
+  addDoctor(addDoctorRequest: AddDoctorRequest): Observable<Doctor> {
+    return this.http.post<Doctor>(`${this.apiUrl}`, addDoctorRequest);
+  }
+
+  updateDoctor(id: string, updateDoctorRequest: UpdateDoctorRequest): Observable<Doctor> {
+    return this.http.put<Doctor>(`${this.apiUrl}/${id}`, updateDoctorRequest);
+  }
+
+  deleteDoctor(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
