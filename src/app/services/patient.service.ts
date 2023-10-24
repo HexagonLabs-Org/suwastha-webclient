00d28df60ddbbcb607935435d1a53a45151b94a9
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-24 16:22:09 +0530

    Update patient.service.ts

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Patient } from '../Models/Entities/patient.model';
@@ -10,27 +10,29 @@ import { AddPatientRequest } from '../Models/DTOs/add-patient-request.model';
   providedIn: 'root'
 })
 export class PatientService {
+  private JWT = localStorage.getItem('JWT');
+  private AuthHeaders = new HttpHeaders().set('Authorization', `Bearer ${this.JWT}`);
   private apiUrl = environment.apiUrl + '/Patient'; // Update this to your API endpoint
 
   constructor(private http: HttpClient) { }
 
   getAllPatients(): Observable<Patient[]> {
-    return this.http.get<Patient[]>(`${this.apiUrl}`);
+    return this.http.get<Patient[]>(`${this.apiUrl}`,{ headers: this.AuthHeaders });
   }
 
   getPatientById(id: string): Observable<Patient> {
-    return this.http.get<Patient>(`${this.apiUrl}/${id}`);
+    return this.http.get<Patient>(`${this.apiUrl}/${id}`,{ headers: this.AuthHeaders });
   }
 
   addPatient(patient: AddPatientRequest): Observable<Patient> {
-    return this.http.post<Patient>(`${this.apiUrl}`, patient);
+    return this.http.post<Patient>(`${this.apiUrl}`, patient,{ headers: this.AuthHeaders });
   }
 
   updatePatient(id: string, patient: UpdatePatientRequest): Observable<Patient> {
-    return this.http.put<Patient>(`${this.apiUrl}/${id}`, patient);
+    return this.http.put<Patient>(`${this.apiUrl}/${id}`, patient,{ headers: this.AuthHeaders });
   }
 
   deletePatient(id: string): Observable<Patient> {
-    return this.http.delete<Patient>(`${this.apiUrl}/${id}`);
+    return this.http.delete<Patient>(`${this.apiUrl}/${id}`,{ headers: this.AuthHeaders });
   }
 }
