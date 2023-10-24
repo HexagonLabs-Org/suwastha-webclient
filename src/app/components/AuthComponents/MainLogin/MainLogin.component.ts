86b55e3be548a8c6bb9e9533f2aef0a55340fd1f
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-24 22:29:39 +0530

    modified main login

--- src/app/components/AuthComponents/MainLogin/MainLogin.component.ts
+++ src/app/components/AuthComponents/MainLogin/MainLogin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,8 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./MainLogin.component.css']
 })
 export class MainLoginComponent {
+  constructor(private router: Router) {}
 
   login(){
     localStorage.setItem('jwtToken', "");
+  }
+
+  goToClinic(){
+    localStorage.setItem('SectionType', "Clinic");
+    this.router.navigate(['/home']);
+  }
+
+  goToOPD(){
+    localStorage.setItem('SectionType', "OPD");
+    this.router.navigate(['/home']);
+  }
+
+  goToETU(){
+    localStorage.setItem('SectionType', "ETU");
+    this.router.navigate(['/home']);
+  }
+
+  goToWard(){
+    localStorage.setItem('SectionType', "Ward");
+    this.router.navigate(['/home']);
   }
 }
