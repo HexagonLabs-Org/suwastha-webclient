81b167a0d5f6f90487f63dc429f8ab4e3fed9a1a
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-24 22:34:23 +0530

    added copen new session link to header

--- src/app/components/LayoutComponents/header/header.component.ts
+++ src/app/components/LayoutComponents/header/header.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class TopbarComponent {
+  SectionType = localStorage.getItem("SectionType");
 
+  openNewSession() {
+    if(this.SectionType == "Clinic"){
+      const url = '/Clinic-Session';
+      window.open(url, '_blank');
+    }
+    else if(this.SectionType == "OPD"){
+      const url = '/OPD-Session';
+      window.open(url, '_blank');
+    }
+
+  }
 }
