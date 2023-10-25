0c97459e1a1f80ddd601a8f5b209c1347f2cbe0f
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-26 00:37:28 +0530

    modified dashboard layout

--- src/app/components/LayoutComponents/dashboard/dashboard.component.ts
+++ src/app/components/LayoutComponents/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  havePrivilegeToUser : boolean = false;
 
+  constructor(private el: ElementRef) {}
+
+  SectionType = localStorage.getItem("SectionType");
+
+  openNewSession() {
+    if(this.SectionType == "Clinic"){
+      const url = '/Clinic-Session';
+      window.open(url, '_blank');
+    }
+    else if(this.SectionType == "OPD"){
+      const url = '/OPD-Session';
+      window.open(url, '_blank');
+    }
+
+  }
+  
+  links = [
+    { text: 'Dashboard', route: '/home' , havePrivilegeToUser:true, icon : 'bxs-dashboard' , active:false},
+    { text: 'Profile', route: '/home/profile', havePrivilegeToUser:true, icon: 'bx-store-alt', active:true},
+    { text: 'Settings', route: '/home/settings', havePrivilegeToUser:true, icon: 'bx-analyse', active:false},
+    { text: 'Patients', route: '/home/patients', havePrivilegeToUser:true, icon: 'bx-message-square-dots', active:false},
+    { text: 'Doctors', route: '/home/doctors', havePrivilegeToUser:true, icon: 'bx-group', active:false},
+    { text: 'Doctors', route: '/home/doctors', havePrivilegeToUser:true, icon: 'bx-cog', active:false}
+  ];
+
+
+  toggleTheme() {
+    const checkbox = this.el.nativeElement.querySelector('#theme-toggle');
+    const body = document.body;
+
+    if (checkbox.checked) {
+      body.classList.add('dark');
+    } else {
+      body.classList.remove('dark');
+    }
+  }
+
+  setActiveLink(clickedLink:any) {
+
+    for (const link of this.links) {
+      link.active = false;
+    }
+    clickedLink.active = true;
+  }
+
+  isSidebarClosed: boolean = false;
+
+  toggleSidebar() {
+    const sideBar = this.el.nativeElement.querySelector('#sidebar');
+    this.isSidebarClosed = !this.isSidebarClosed;
+    if(this.isSidebarClosed){
+      sideBar.classList.add('close');
+    }
+    else{
+      sideBar.classList.remove('close');
+    }
+    console.log(this.isSidebarClosed);
+  }
 }
