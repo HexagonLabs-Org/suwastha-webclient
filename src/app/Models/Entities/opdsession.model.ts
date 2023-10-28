be0ca280069baebcad15c2f75cf8c258c6e2afb1
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-10-28 15:42:28 +0530

    add assets

--- src/app/Models/Entities/opdsession.model.ts
+++ src/app/Models/Entities/opdsession.model.ts
@@ -1,13 +1,13 @@
 export interface OpdsessionModel{
-    id: string | null;
-    sessionID: string | null;
-    opdID: string | null;
-    doctorID: string | null;
-    startedAt: Date | null;
-    description: string | null;
-    specialNotes: string | null;
-    closedAt: Date | null;
-    isActive: boolean | null;
-    closingNote: string | null;
+    id?: string | null;
+    sessionID?: string | null;
+    opdid?: string | null;
+    doctorID?: string | null;
+    startedAt?: Date | null;
+    description?: string | null;
+    specialNotes?: string | null;
+    closedAt?: Date | null;
+    isActive?: boolean | null;
+    closingNote?: string | null;
 }
 
